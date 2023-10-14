7945a657caa2fa65a8785c4cd82f804eb4ce1927
znhy1024 <103940205+znhy1024@users.noreply.github.com>
2023-10-14 16:39:30 +0800

    Update run_fs.sh

--- run_fs.sh
+++ run_fs.sh
@@ -8,7 +8,7 @@ for ds in fever scifact vc
 do
     for seed in 0 2 3 4
     do
-    python -u pl_train.py -k  exp_name=${ds}_shots${shot}_seed${seed}_fs few_shot_random_seed=${seed} seed=${seed} dataset=${ds} batch_size=1 grad_accum_factor=2 num_steps=1500 eval_batch_size=4 num_shot=12 stage=2 load_weight=pretrained_checkpoints/warmup/${ds}_shots${shot}_seed${seed}.pt
-    python -u pl_train.py -k  exp_name=${ds}_shots${shot}_seed${seed}_fs few_shot_random_seed=${seed} seed=${seed} dataset=${ds}  save_model=false num_steps=0 eval_before_training=True eval_batch_size=4 num_shot=12 stage=2 load_weight=${OUTPUT_PATH}/${ds}_shots${shot}_seed${seed}_fs/finish.pt
+    python -u pl_train.py -k  exp_name=${ds}_shots12_seed${seed}_fs few_shot_random_seed=${seed} seed=${seed} dataset=${ds} batch_size=1 grad_accum_factor=2 num_steps=1500 eval_batch_size=4 num_shot=12 stage=2 load_weight=pretrained_checkpoints/warmup/${ds}_shots12_seed${seed}.pt
+    python -u pl_train.py -k  exp_name=${ds}_shots12_seed${seed}_fs few_shot_random_seed=${seed} seed=${seed} dataset=${ds} save_model=false num_steps=0 eval_before_training=True eval_batch_size=4 num_shot=12 stage=2 load_weight=${OUTPUT_PATH}/${ds}_shots12_seed${seed}_fs/finish.pt
     done
 done
